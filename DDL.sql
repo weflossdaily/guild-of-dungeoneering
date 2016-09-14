97266a19b2c73f34664af776256ff88bf512c41f
Reid <weflossdaily@gmail.com>
2016-09-13 20:03:35 -0400

    Update
    Add (NOT) NULL where appropriate, add UNIQUE CONSTRAINTs, add Effect
    TABLE and CardEffect mappings, add Scar TABLE and AdventureDependency
    TABLE, add comment field to Quest (so you can keep notes, e.g., when
    you start on an island or if it’s possible to avoid certain critters,
    etc.), some DML updates

--- DDL.sql
+++ DDL.sql
@@ -1,165 +1,203 @@
 CREATE TABLE Region(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50)
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Adventure(
-    Id INTEGER PRIMARY KEY ASC,
-    RegionId INTEGER,
-    Name VARCHAR(50),
-    FOREIGN KEY(RegionId) REFERENCES Region(Id)
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    RegionId INTEGER NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    FOREIGN KEY(RegionId) REFERENCES Region(Id),
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Consequence(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50)
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Quest(
-    Id INTEGER PRIMARY KEY ASC,
-    AdventureId INTEGER,
-    Name VARCHAR(50),
-    Bonus INTEGER,
-    LastAward INTEGER,
-    Objective VARCHAR(200),
-    BossChase INTEGER,
-    TurnLimit INTEGER,
-    LimitConsequenceId INTEGER,
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    AdventureId INTEGER NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Bonus INTEGER NOT NULL,
+    LastAward INTEGER NULL,
+    Objective VARCHAR(200) NOT NULL,
+    BossChase INTEGER NULL,
+    TurnLimit INTEGER NULL,
+    LimitConsequenceId INTEGER NULL,
+    Comment VARCHAR(1000) NULL,
     FOREIGN KEY(AdventureId) REFERENCES Adventure(Id),
-    FOREIGN KEY(LimitConsequenceId) REFERENCES Consequence(Id)
+    FOREIGN KEY(LimitConsequenceId) REFERENCES Consequence(Id),
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Monster(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50),
-    Level INTEGER,
-    Hearts INTEGER
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Level INTEGER NOT NULL,
+    Hearts INTEGER NOT NULL,
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE MonsterQuest(
-    MonsterId INTEGER,
-    QuestId INTEGER,
+    MonsterId INTEGER NOT NULL,
+    QuestId INTEGER NOT NULL,
     FOREIGN KEY(MonsterId) REFERENCES Monster(Id),
-    FOREIGN KEY(QuestId) REFERENCES Quest(Id)
+    FOREIGN KEY(QuestId) REFERENCES Quest(Id),
+    CONSTRAINT MonsterQuestUnique UNIQUE (MonsterId,QuestId)
 );
 
 CREATE TABLE Dungeoneer(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50),
-    Hearts INTEGER,
-    UnlockCost INTEGER
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Hearts INTEGER NOT NULL,
+    UnlockCost INTEGER NOT NULL,
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Discipline(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50)
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Trait(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50),
-    Description VARCHAR(200)
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Description VARCHAR(200) NOT NULL,
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Rarity(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50)
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Tier(
-    Id INTEGER PRIMARY KEY ASC,
-    Level INTEGER,
-    Cost INTEGER
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Level INTEGER NOT NULL,
+    Cost INTEGER NOT NULL
 );
 
 CREATE TABLE GuildUpgrade(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50),
-    TierId INTEGER,
-    FOREIGN KEY(TierId) REFERENCES Tier(Id)
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    TierId INTEGER NOT NULL,
+    FOREIGN KEY(TierId) REFERENCES Tier(Id),
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE Item(
-    Id INTEGER PRIMARY KEY ASC,
-    Name VARCHAR(50),
-    GuildUpgradeId INTEGER,
-    RarityId INTEGER,
-    Level INTEGER,
-    Hearts INTEGER,
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    GuildUpgradeId INTEGER NOT NULL,
+    RarityId INTEGER NOT NULL,
+    Level INTEGER NOT NULL,
+    Hearts INTEGER NOT NULL,
     FOREIGN KEY(GuildUpgradeId) REFERENCES GuildUpgrade(Id),
-    FOREIGN KEY(RarityId) REFERENCES Rarity(Id)
+    FOREIGN KEY(RarityId) REFERENCES Rarity(Id),
+    CONSTRAINT NameUnique UNIQUE (Name)
 );
 
 CREATE TABLE MonsterDiscipline(
-    MonsterId INTEGER,
-    DisciplineId INTEGER,
-    Level INTEGER,
+    MonsterId INTEGER NOT NULL,
+    DisciplineId INTEGER NOT NULL,
+    Level INTEGER NOT NULL,
     FOREIGN KEY(MonsterId) REFERENCES Monster(Id),
-    FOREIGN KEY(DisciplineId) REFERENCES Discipline(Id)
+    FOREIGN KEY(DisciplineId) REFERENCES Discipline(Id),
+    CONSTRAINT MonsterDisciplineUnique UNIQUE (MonsterId,DisciplineId)
 );
 
 CREATE TABLE ItemTrait(
-    ItemId INTEGER,
-    TraitId INTEGER,
+    ItemId INTEGER NOT NULL,
+    TraitId INTEGER NOT NULL,
     FOREIGN KEY(ItemId) REFERENCES Item(Id),
-    FOREIGN KEY(TraitId) REFERENCES Trait(Id)
+    FOREIGN KEY(TraitId) REFERENCES Trait(Id),
+    CONSTRAINT ItemTraitUnique UNIQUE (ItemId,TraitId)
 );
 
 CREATE TABLE MonsterTrait(
-    MonsterId INTEGER,
-    TraitId INTEGER,
+    MonsterId INTEGER NOT NULL,
+    TraitId INTEGER NOT NULL,
     FOREIGN KEY(MonsterId) REFERENCES Monster(Id),
-    FOREIGN KEY(TraitId) REFERENCES Trait(Id)
+    FOREIGN KEY(TraitId) REFERENCES Trait(Id),
+    CONSTRAINT MonsterTraitUnique UNIQUE (MonsterId,TraitId)
 );
 
 CREATE TABLE DungeoneerTrait(
-    DungeoneerId INTEGER,
-    TraitId INTEGER,
+    DungeoneerId INTEGER NOT NULL,
+    TraitId INTEGER NOT NULL,
     FOREIGN KEY(DungeoneerId) REFERENCES Dungeoneer(Id),
-    FOREIGN KEY(TraitId) REFERENCES Trait(Id)
+    FOREIGN KEY(TraitId) REFERENCES Trait(Id),
+    CONSTRAINT DungeoneerTraitUnique UNIQUE (DungeoneerId,TraitId)
 );
 
 CREATE TABLE ItemDiscipline(
-    ItemId INTEGER,
-    DisciplineId INTEGER,
-    Level INTEGER,
+    ItemId INTEGER NOT NULL,
+    DisciplineId INTEGER NOT NULL,
+    Level INTEGER NOT NULL,
     FOREIGN KEY(ItemId) REFERENCES Item(Id),
-    FOREIGN KEY(DisciplineId) REFERENCES Discipline(Id)
+    FOREIGN KEY(DisciplineId) REFERENCES Discipline(Id),
+    CONSTRAINT ItemDisciplineUnique UNIQUE (ItemId,DisciplineId)
 );
 
 CREATE TABLE Card(
+    Id INTEGER PRIMARY KEY ASC NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    CONSTRAINT NameUnique UNIQUE (Name)
+);
+
+CREATE TABLE Effect(
     Id INTEGER PRIMARY KEY ASC,
     Name VARCHAR(50),
-    PhysicalDamage INTEGER,
-    MagicDamage INTEGER,
-    Quick INTEGER,
-    Unblockable INTEGER,
-    PhysicalBlocks INTEGER,
-    MagicBlocks INTEGER,
-    AllBlock INTEGER,
-    StealOnSuccess INTEGER,
-    ConcealOnSuccess INTEGER,
-    CloneOnSuccess INTEGER,
-    DiscardOnSuccess INTEGER,
-    Hearts INTEGER,
-    AddedPhysicalToNext INTEGER,
-    AddedMagicToNext INTEGER,
-    Swap INTEGER,
-    Draw INTEGER
+    Description VARCHAR(200),
+    CONSTRAINT NameUnique UNIQUE (Name)
+);
+
+CREATE TABLE CardEffect(
+    CardId INTEGER NOT NULL,
+    EffectId INTEGER NOT NULL,
+    Quantity INTEGER NULL,
+    NeedsSuccess INTEGER NULL,
+    FOREIGN KEY(CardId) REFERENCES Card(Id),
+    FOREIGN KEY(EffectId) REFERENCES Effect(Id),
+    CONSTRAINT CardEffectUnique UNIQUE (CardId, EffectId)
 );
 
 CREATE TABLE DisciplineCard(
-    DisciplineId INTEGER,
-    CardId INTEGER,
-    Level INTEGER,
+    DisciplineId INTEGER NOT NULL,
+    CardId INTEGER NOT NULL,
+    Level INTEGER NOT NULL,
     FOREIGN KEY(DisciplineId) REFERENCES Discipline(Id),
-    FOREIGN KEY(CardId) REFERENCES Card(Id)
+    FOREIGN KEY(CardId) REFERENCES Card(Id),
+    CONSTRAINT DisciplineCardUnique UNIQUE (DisciplineId,CardId)
 );
 
 CREATE TABLE DungeoneerCard(
-    DungeoneerId INTEGER,
-    CardId INTEGER,
-    Quantity INTEGER,
+    DungeoneerId INTEGER NOT NULL,
+    CardId INTEGER NOT NULL,
+    Quantity INTEGER NOT NULL,
     FOREIGN KEY(DungeoneerId) REFERENCES Dungeoneer(Id),
-    FOREIGN KEY(CardId) REFERENCES Card(Id)
+    FOREIGN KEY(CardId) REFERENCES Card(Id),
+    CONSTRAINT DungeoneerCard UNIQUE (DungeoneerId,CardId)
+);
+
+CREATE TABLE Scar(
+    Id INTEGER PRIMARY KEY ASC,
+    Name VARCHAR(50),
+    EarlyStageDescription VARCHAR(200),
+    FinalStageDescription VARCHAR(200),
+    CONSTRAINT NameUnique UNIQUE (Name)
+);
+
+CREATE TABLE AdventureDependency(
+    ParentAdventureId INTEGER NOT NULL,
+    ChildAdventureId INTEGER NOT NULL,
+    FOREIGN KEY(ParentAdventureId) REFERENCES Adventure(Id),
+    FOREIGN KEY(ChildAdventureId) REFERENCES Adventure(Id),
+    CONSTRAINT ParentChildUnique UNIQUE (ParentAdventureId,ChildAdventureId)
 );
